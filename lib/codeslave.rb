a34380b01d1427f551e37662d8155bbdffce9b77
Curt Howard <curt@portugly.com>
2019-12-18 08:36:10 -0800

    wip

--- lib/codeslave.rb
+++ lib/codeslave.rb
@@ -1,6 +1,22 @@
-require "codeslave/version"
+require 'slop'
+
+require 'codeslave/version'
 
 module Codeslave
   class Error < StandardError; end
-  # Your code goes here...
+
+  class CLI
+    def initialize(args)
+      @options = Slop.parse(args) do |option|
+        option.bool '-h', '--help', 'print this help'
+        option.on '-v', '--version', 'print the version'
+      end
+
+      puts @options if @options.help?
+      puts "Codeslave v#{Codeslave::VERSION}" if @options.version?
+    end
+
+    def run
+    end
+  end
 end
